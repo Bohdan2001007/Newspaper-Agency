422e6e2921419fb5f300b7b6baee9411eb5880ce
Bohdan2001007 <bogdan1991007@gmail.com>
2023-02-20 19:50:46 +0100

    Added .env file

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SECRET_KEY = os.environ.get
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,8 +24,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-eh0o2dc+zh6hw6dhc!_%#!sao#km^e_nshaqr3v8*tzzdmv55u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
